6dc18c758abd2da34ec68a5269d62119b83036b5
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2025-05-28 15:48:03 -0300

    desaparece la merca

---
+++ Assets/DeteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto"+collision.gameObject.name);
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/MercaScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercaScript : MonoBehaviour
+{
+    public int precio;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
